7d090224475f0413198537ab85f2d2ab4b5afdec
Yash Dixit <ydixit007@gmail.com>
2024-04-26 20:09:38 +0530

    added user in navbar

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,7 @@
 const Loading = () => {
   return (
     <div className='min-h-screen flex justify-center items-center black text-2xl'>
-        <h1 className='bg-primary px-4 py-2'>Loading...</h1>
+        <h1 className='bg-primary px-4 py-2 text-neutral'>Loading...</h1>
     </div>
   )
 }
